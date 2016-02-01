8fed94326e57d0698c983b95e5a823c1d3fba2d8
kikuchy <kikuchy.h@gmail.com>
2016-02-02 00:53:04 +0900

    add ValidationResult

--- kenin/src/main/java/kikuchy/net/kenin/Condition.java
+++ kenin/src/main/java/kikuchy/net/kenin/Condition.java
@@ -3,5 +3,6 @@ package kikuchy.net.kenin;
 /**
  * Created by hiroshi.kikuchi on 2016/02/01.
  */
-public class Condition {
+public abstract class Condition<T, E> {
+    public abstract ValidationResult<E> validate(T value);
 }
