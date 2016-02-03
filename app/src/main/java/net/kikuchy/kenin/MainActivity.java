54d938ab2933b72a474e0d8b8e8571139f2c29bd
kikichy <kikuchy.h@gmail.com>
2016-02-03 14:38:09 +0900

    sample implementaion

--- app/src/main/java/net/kikuchy/kenin/MainActivity.java
+++ app/src/main/java/net/kikuchy/kenin/MainActivity.java
@@ -1,19 +1,24 @@
 package net.kikuchy.kenin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+
+import net.kikuchy.kenin.internal.RequireCondition;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mUserId;
+    private TextInputLayout mUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        KeninAndroid.builder(mUserId)
-                .condition()
-                .build();
+        mUserId = (TextInputLayout) findViewById(R.id.user_id);
+
+        KeninAndroid.
+                builder(mUserId).
+                setCondition(new RequireCondition()).
+                build();
     }
 }
