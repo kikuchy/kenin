a1b189036f3696787166249d90c749dc7993fb3b
kikichy <kikuchy.h@gmail.com>
2016-02-03 14:28:18 +0900

    remove CompositeCondition temporary

---
+++ kenin-java/src/main/java/net/kikuchy/kenin/CompositeCondition.java
@@ -0,0 +1,9 @@
+package net.kikuchy.kenin;
+
+import java.util.List;
+
+/**
+ * Created by hiroshi.kikuchi on 2016/02/01.
+ */
+public abstract class CompositeCondition<T> implements Condition<T> {
+}
