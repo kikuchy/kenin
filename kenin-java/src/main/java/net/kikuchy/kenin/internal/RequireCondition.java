9fe3a64c88f92cb78e1da9f57df0ed1a3533b557
kikuchy <kikuchy.h@gmail.com>
2016-02-04 22:02:06 +0900

    make RequireCondition can custom error message

--- kenin-java/src/main/java/net/kikuchy/kenin/internal/RequireCondition.java
+++ kenin-java/src/main/java/net/kikuchy/kenin/internal/RequireCondition.java
@@ -6,22 +6,32 @@ import net.kikuchy.kenin.ErrorMessageCollection;
 import net.kikuchy.kenin.ValidationResult;
 
 /**
- * Created by kikuchy on 16/02/03.
+ * This condition requires a value.
  */
 public class RequireCondition implements Condition<CharSequence> {
-    private final static ErrorMessage MESSAGE = new ErrorMessage() {
-        @Override
-        public String toString() {
-            return "This field is required.";
-        }
-    };
+    private final static String DEFAULT_MESSAGE = "This field is required.";
+
+    private final ErrorMessage message;
+
+    public RequireCondition(final String errorMessage) {
+        this.message = new ErrorMessage() {
+            @Override
+            public String toString() {
+                return errorMessage;
+            }
+        };
+    }
+
+    public RequireCondition() {
+        this(DEFAULT_MESSAGE);
+    }
 
     @Override
     public ValidationResult validate(CharSequence value) {
         ErrorMessageCollection errors = new ErrorMessageCollection();
         boolean isValid = value.length() != 0;
         if (!isValid)
-            errors.add(MESSAGE);
+            errors.add(message);
         return new ValidationResult(isValid, errors);
     }
 }
