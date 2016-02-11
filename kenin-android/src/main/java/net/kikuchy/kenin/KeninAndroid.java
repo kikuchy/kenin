99d0a2f85165637b7632bdad6b5c459d561db6f8
kikuchy <kikuchy.h@gmail.com>
2016-02-12 00:55:00 +0900

    fix KeninAndroid for Condition

--- kenin-android/src/main/java/net/kikuchy/kenin/KeninAndroid.java
+++ kenin-android/src/main/java/net/kikuchy/kenin/KeninAndroid.java
@@ -7,6 +7,8 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 
 import net.kikuchy.kenin.result.CachedResultReceiver;
+import net.kikuchy.kenin.result.ErrorReason;
+import net.kikuchy.kenin.result.ResultReceiver;
 import net.kikuchy.kenin.trigger.ValueChangedEventRelay;
 
 import java.util.List;
@@ -14,16 +16,16 @@ import java.util.List;
 /**
  * Created by hiroshi.kikuchi on 2016/02/01.
  */
-public class KeninAndroid<T> extends Kenin<T> {
+public class KeninAndroid<V, E> extends Kenin<V, E> {
     protected Context context;
 
-    protected KeninAndroid(Builder<T> builder) {
+    protected KeninAndroid(Builder<V, E> builder) {
         super(builder);
         this.context = builder.context;
     }
 
-    public static Kenin.Builder<CharSequence> builder(final EditText editText) {
-        return new Builder<>(new net.kikuchy.kenin.trigger.ValueChangedEventRelay<CharSequence>() {
+    public static <E> Kenin.Builder<CharSequence, E> builder(final EditText editText) {
+        return new Builder<>(new ValueChangedEventRelay<CharSequence>() {
             @Override
             public void relay(final net.kikuchy.kenin.trigger.ValueChangedEventEmitter<CharSequence> emitter) {
                 editText.addTextChangedListener(new TextWatcher() {
@@ -46,29 +48,29 @@ public class KeninAndroid<T> extends Kenin<T> {
         }, editText.getContext());
     }
 
-    public static Kenin.Builder<CharSequence> builder(final TextInputLayout textInputLayout) {
-        return builder(textInputLayout.getEditText())
-                .addResultReceiver(new CachedResultReceiver(new net.kikuchy.kenin.result.ResultReceiver() {
+    public static Kenin.Builder<CharSequence, String> builder(final TextInputLayout textInputLayout) {
+        return KeninAndroid.<String>builder(textInputLayout.getEditText())
+                .addResultReceiver(new CachedResultReceiver<>(new ResultReceiver<String>() {
                     @Override
                     public void validationSucceeded() {
                         textInputLayout.setError(null);
                     }
 
                     @Override
-                    public void validationFailed(List<String> errorMessages) {
+                    public void validationFailed(List<ErrorReason<String>> errorMessages) {
                         StringBuilder builder = new StringBuilder();
-                        for (String err : errorMessages) {
+                        for (ErrorReason<String> err : errorMessages) {
                             if (builder.length() != 0) {
                                 builder.append('\n');
                             }
-                            builder.append(err);
+                            builder.append(err.getReason());
                         }
                         textInputLayout.setError(builder.toString());
                     }
                 }));
     }
 
-    public static class Builder<T> extends Kenin.Builder<T> {
+    public static class Builder<T, E> extends Kenin.Builder<T, E> {
         private Context context;
 
         protected Builder(ValueChangedEventRelay<T> relay, Context context) {
@@ -76,7 +78,7 @@ public class KeninAndroid<T> extends Kenin<T> {
             this.context = context;
         }
 
-        public Builder<T> setContext(Context context) {
+        public Builder<T, E> setContext(Context context) {
             this.context = context;
             return this;
         }
