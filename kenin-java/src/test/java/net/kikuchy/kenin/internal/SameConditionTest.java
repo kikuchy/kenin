0045f37bb6ffd2c9f98e35d4667a18f1c02de878
kikuchy <kikuchy.h@gmail.com>
2016-02-12 23:32:01 +0900

    add SameConditionTest lazy pattern

--- kenin-java/src/test/java/net/kikuchy/kenin/internal/SameConditionTest.java
+++ kenin-java/src/test/java/net/kikuchy/kenin/internal/SameConditionTest.java
@@ -20,4 +20,30 @@ public class SameConditionTest {
         ValidationResult<String> two = condition.validate("foobar");
         assertTrue(two.isValid());
     }
+
+    @Test
+    public void testValidateLazy() throws Exception {
+        TestGetter<CharSequence> lazy = new TestGetter<>();
+        Condition<CharSequence, String> condition = new SameCondition<>(lazy, "be same");
+        lazy.setValue("foobar");
+        ValidationResult<String> one = condition.validate("hogefuga");
+        assertFalse(one.isValid());
+        ValidationResult<String> two = condition.validate("foobar");
+        assertTrue(two.isValid());
+        lazy.setValue("Keep Calm and I'm sleeping");
+        ValidationResult<String> three = condition.validate("Keep Calm and I'm sleeping");
+        assertTrue(three.isValid());
+    }
+
+    static class TestGetter<T> implements SameCondition.LazyGetter<T> {
+        private T value;
+        public void setValue(T v) {
+            value = v;
+        }
+
+        @Override
+        public T get() {
+            return value;
+        }
+    }
 }
