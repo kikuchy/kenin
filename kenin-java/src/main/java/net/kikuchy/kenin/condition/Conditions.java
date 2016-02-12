5b0cf4bbd1d51590ede6924a4c7bb6a41ae8907e
kikichy <kikuchy.h@gmail.com>
2016-02-12 14:25:28 +0900

    add regexp pattern matching condition

--- kenin-java/src/main/java/net/kikuchy/kenin/condition/Conditions.java
+++ kenin-java/src/main/java/net/kikuchy/kenin/condition/Conditions.java
@@ -1,5 +1,6 @@
 package net.kikuchy.kenin.condition;
 
+import net.kikuchy.kenin.internal.PatternMatchCondition;
 import net.kikuchy.kenin.internal.RequireCondition;
 import net.kikuchy.kenin.internal.SameCondition;
 
@@ -21,5 +22,14 @@ public final class Conditions {
 
     public static Condition<CharSequence, String> same(CharSequence expected, String errorMessage) {
         return new SameCondition<>(expected, errorMessage);
+    }
+
+    public static Condition<CharSequence, String> pattern(String pattern) {
+        return new PatternMatchCondition<>(pattern,
+                String.format("Value must be match with the pattern \"%s\"", pattern));
+    }
+
+    public static Condition<CharSequence, String> pattern(String pattern, String errorMessage) {
+        return new PatternMatchCondition<>(pattern, errorMessage);
     }
 }
---
+++ kenin-java/src/test/java/net/kikuchy/kenin/internal/PatternMatchConditionTest.java
@@ -0,0 +1,25 @@
+package net.kikuchy.kenin.internal;
+
+import net.kikuchy.kenin.condition.Condition;
+import net.kikuchy.kenin.result.ValidationResult;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by hiroshi.kikuchi on 2016/02/12.
+ */
+public class PatternMatchConditionTest {
+
+    @Test
+    public void testValidate() throws Exception {
+        Condition<CharSequence, String> condition =
+                new PatternMatchCondition<>("^abc\\def", "not matched");
+        ValidationResult<String> one = condition.validate("abc4efghijklmnop");
+        assertTrue(one.isValid());
+        ValidationResult<String> two = condition.validate("abcdefghijklmnop");
+        assertFalse(two.isValid());
+    }
+}
