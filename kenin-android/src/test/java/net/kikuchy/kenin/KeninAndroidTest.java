b7c11600c686695544e692dcdc47f7b9191f3c91
kikuchy <kikuchy.h@gmail.com>
2016-02-20 20:07:45 +0900

    add unit test for kenin-android

---
+++ kenin-android/src/test/java/net/kikuchy/kenin/KeninAndroidTest.java
@@ -0,0 +1,59 @@
+package net.kikuchy.kenin;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.EditText;
+
+import net.kikuchy.kenin.condition.Conditions;
+import net.kikuchy.kenin.result.ResultReceiver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class KeninAndroidTest {
+    Context getContext() {
+        return InstrumentationRegistry.getTargetContext();
+    }
+
+    @Test
+    public void editTextValidationFailed() throws Exception {
+        EditText et = new EditText(getContext());
+        et.setText("a");
+        KeninAndroid.create(et, Conditions.requireField()).addResultReceiver(new ResultReceiver<String>() {
+            @Override
+            public void validationSucceeded() {
+                assertTrue(false);
+            }
+
+            @Override
+            public void validationFailed(List<String> list) {
+                assertTrue(true);
+            }
+        });
+        et.setText("");
+    }
+
+    @Test
+    public void editTextValidationSucceeded() throws Exception {
+        EditText et = new EditText(getContext());
+        et.setText("");
+        KeninAndroid.create(et, Conditions.requireField()).addResultReceiver(new ResultReceiver<String>() {
+            @Override
+            public void validationSucceeded() {
+                assertTrue(true);
+            }
+
+            @Override
+            public void validationFailed(List<String> list) {
+                assertTrue(false);
+            }
+        });
+        et.setText("a");
+    }
+}
